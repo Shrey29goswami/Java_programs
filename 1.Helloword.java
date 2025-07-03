0567a6dc33cad97d0f934e89d13d7f83201a1bc9
Shrey Goswami <140746638+Shrey29goswami@users.noreply.github.com>
2025-07-03 12:13:30 +0530

    Delete 1.Helloword.java

--- 1.Helloword.java
+++
@@ -1,9 +0,0 @@
-public class helloworld
-{
- public static void main(String args[])
- { 
-   System.out.println("Hello world!");
-   System.out.print("Hello again.");
-   System.out.print("Bu22csen0100751");
- }
-}
